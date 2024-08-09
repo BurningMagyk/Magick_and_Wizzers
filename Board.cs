e41e9a0586b7809d91fbe51f2b72bfb4bca85057
Robin Campos <magyk81@gmail.com>
2024-08-08 19:44:34 -0600

    Add UI class

--- Board.cs
+++ Board.cs
@@ -4,13 +4,23 @@ using System;
 namespace Main {
 	public partial class Board : Node
 	{
-		private Sprite2D _baseTile;
+		private PackedScene _baseTile;
+		private Sprite2D[,] tiles;
 
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready() {
-			_baseTile = GetNode<Sprite2D>("Tile");
-			_baseTile.scri
-			// GD.Load()
+			_baseTile = ResourceLoader.Load<PackedScene>("res://Tile.tscn");
+			
+			tiles = new Sprite2D[7, 7];
+			for (int i = 0; i < tiles.GetLength(0); i++) {
+				for (int j = 0; j < tiles.GetLength(1); j++) {
+					Sprite2D tile = (Sprite2D) _baseTile.Instantiate();
+					tile.Position = new Vector2(i * tile.Transform.Scale.X, j * tile.Transform.Scale.Y);
+					tiles[i, j] = tile;
+				}
+			}
+
+			// _baseTile.visible = false;
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
