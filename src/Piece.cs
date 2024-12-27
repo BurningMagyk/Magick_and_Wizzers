8f2f1c43e1a6fc1be78019c6a4fc8c500f61af68
Burning Magyk <magyk81@gmail.com>
2024-12-26 23:39:35 -0700

    Add to monster pool

--- src/Piece.cs
+++ src/Piece.cs
@@ -4,24 +4,24 @@ using System;
 namespace Main {
   public partial class Piece : MeshInstance3D
   {
-    public Tile Tile {
-        get => tile;
-        set {
-          tile = value;
-          Position = tile.GlobalPosition;
-          // Scale = value.GlobalScale;
-        }
-      }
-      public Stats Stats { get; set; }
+	public Tile Tile {
+		get => tile;
+		set {
+		  tile = value;
+		  Position = tile.GlobalPosition;
+		  // Scale = value.GlobalScale;
+		}
+	  }
+	  public Stats Stats { get; set; }
 
-      private Tile tile;
+	  private Tile tile;
 
-      // Called when the node enters the scene tree for the first time.
-      public override void _Ready() {
-      }
+	  // Called when the node enters the scene tree for the first time.
+	  public override void _Ready() {
+	  }
 
-      // Called every frame. 'delta' is the elapsed time since the previous frame.
-      public override void _Process(double delta) {
-      }
+	  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	  public override void _Process(double delta) {
+	  }
   }
 }
